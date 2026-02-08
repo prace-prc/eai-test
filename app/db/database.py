d730ee6ad28e353b494ec5da83680d2c1669b1b2
Juwon Hwang <wndnjs719@gmail.com>
2026-02-08 17:32:34 +0900

    환경변수 오타 수정

--- app/db/database.py
+++ app/db/database.py
@@ -1,10 +1,18 @@
 import os
-from sqlalchemy import create_engine, text
+from sqlalchemy import create_engine
 from dotenv import load_dotenv
 
 load_dotenv()
 
-ORDER_TB_URL = f"mysql+pymysql://{os.getenv('ORDER_TB_USER')}:{os.getenv('ORDER_TB_PASS')}@" \
-               f"{os.getenv('ORDER_TB_HOST')}:{os.getenv('ORDER_TB_PORT')}/{os.getenv('ORDER_TB_NAME')}"
+ORDER_TB_URL = f"mysql+pymysql://{os.getenv('ORDER_TB_USER')}:{os.getenv('ORDER_TB_PASSWORD')}@" \
+               f"{os.getenv('ORDER_TB_HOST')}:{os.getenv('ORDER_TB_PORT')}/{os.getenv('ORDER_TB_TABLENAME')}?charset=utf8mb4"
 
-engine = create_engine(ORDER_TB_URL, echo=True)
+print(ORDER_TB_URL)
+
+engine = create_engine(
+    ORDER_TB_URL,
+    pool_pre_ping=True,  # 죽은 커넥션 자동 체크
+    connect_args={
+        "ssl": {}
+    }
+)
--- run.py
+++ run.py
@@ -1,1 +1,6 @@
-print('Project Ready')
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+print(os.getenv('ORDER_TB_HOST'), os.getenv('ORDER_TB_PORT'), os.getenv('ORDER_TB_SID'))
