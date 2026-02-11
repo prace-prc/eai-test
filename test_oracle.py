6bdc0f3003434a61fb419dd66783b23b6e2acb5e
Juwon Hwang <wndnjs719@gmail.com>
2026-02-11 19:13:00 +0900

    불필요한 파일 정리

--- test_oracle.py
+++
@@ -1,6 +0,0 @@
-from app.db.database import engine
-from sqlalchemy import text
-
-with engine.connect() as conn:
-    result = conn.execute(text("SELECT 1 FROM DUAL"))
-    print("Oracle 연결 성공 ✅", result.scalar())
